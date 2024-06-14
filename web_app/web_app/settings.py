dd952bdb788b5ef85b32777ad7f311f4b943fa28
bioqu <lpereirc@gmail.com>
2024-06-13 22:56:35 -0400

    git branch --unset-upstream

--- web_app/web_app/settings.py
+++ web_app/web_app/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "api.app.ApiConfig"
+    "api.app.ApiConfig",
 ]
 
 MIDDLEWARE = [
